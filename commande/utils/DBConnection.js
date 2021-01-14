ea445bc9c6642baebe7c8dac1485b2e3e07b789c
David Baraniak <engenteur@davidbaraniak.fr>
2021-01-14 15:16:01 +0100

    Importation du code d'avant

---
+++ commande/utils/DBConnection.js
@@ -0,0 +1,16 @@
+const mysql = require("mysql");
+
+const dbconnection = mysql.createConnection({
+    host: process.env.MYSQL_HOST,
+    database: process.env.MYSQL_DATABASE,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD
+});
+
+dbconnection.connect((err) => {
+    if (err)
+        throw err;
+    console.log(`Connected to DB "${process.env.MYSQL_DATABASE}"`);
+});
+
+module.exports = dbconnection;
