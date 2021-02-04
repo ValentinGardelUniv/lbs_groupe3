53eaa972c22110c30051c78f98182ced37c58641
David Baraniak <engenteur@davidbaraniak.fr>
2021-02-04 14:35:22 +0100

    TD3 fini

--- commande/routes/commandesroutes.js
+++ commande/routes/commandesroutes.js
@@ -31,17 +31,43 @@ router.get("/", async (req, res, next) => {
 }).post("/", jsonparser, async (req, res, next) => {
     try {
         if (!(req.body.nom_client && req.body.mail_client && req.body.date_livraison))
-            throw new Error();
-        //const commande = await dbclient.one("SELECT id, created_at, mail, montant FROM commande WHERE id = '"+req.params.id+"'");
-        return res.json("coucou");
-        if (commande)
-            return res.json({
+            next(500);
+        let dateaujourdhui = new Date();
+        let journeeaujourdhui = dateaujourdhui.getDate();
+        if (journeeaujourdhui < 10)
+            journeeaujourdhui = "0" + journeeaujourdhui;
+        let moisaujourdhui = dateaujourdhui.getMonth() + 1;
+        if (moisaujourdhui < 10)
+            moisaujourdhui = "0" + moisaujourdhui;
+        let heureaujourdhui = dateaujourdhui.getHours();
+        if (heureaujourdhui < 10)
+            heureaujourdhui = "0" + heureaujourdhui;
+        let minutesaujourdhui = dateaujourdhui.getMinutes();
+        if (minutesaujourdhui < 10)
+            minutesaujourdhui = "0" + minutesaujourdhui;
+        let secondesaujourdhui = dateaujourdhui.getSeconds();
+        if (secondesaujourdhui < 10)
+            secondesaujourdhui = "0" + secondesaujourdhui;
+        let nouveauid = "fzefz";
+        let datetimecreation = dateaujourdhui.getFullYear() + "-" + moisaujourdhui + "-" + journeeaujourdhui + " " + heureaujourdhui + ":" + minutesaujourdhui + ":" + secondesaujourdhui;
+        await dbclient.query("INSERT INTO commande (id, created_at, livraison, nom, mail) VALUES ('"+nouveauid+"', '"+datetimecreation+"', '"+req.body.date_livraison+"', '"+req.body.nom_client+"', '"+req.body.mail_client+"')");
+        const commande = await dbclient.one("SELECT id, created_at, livraison, nom, mail, montant FROM commande WHERE id = '"+nouveauid+"'");
+        if (commande) {
+            res.set('Location', '/commandes/'+nouveauid);
+            return res.status(201).json({
                 type: "resource",
-                commande: commande
+                commande: {
+                    id: commande.id,
+                    mail_client: commande.mail,
+                    nom_client: commande.nom,
+                    date_commande: commande.created_at,
+                    date_livraison: commande.livraison,
+                    montant: commande.montant
+                }
             });
+        }
         return handler404(res);
     } catch(err) {
-        console.log("dsq");
         next(500);
     }
 }).all(handler405);
